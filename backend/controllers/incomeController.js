2875aaef54b0cf2ee162e7806115ffcfb8edca3f
Pranjal Kamboj <pranjalkamboj007@gmail.com>
2023-04-15 20:56:14 +0530

    income and expense controllers

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -2,9 +2,13 @@ const Income = require("../models/incomeModel");
 
 exports.addIncome = async (req, res) => {
   try {
-    //validatons ...
-
     const { amount, category, date, description } = req.body;
+
+    //validatons ...
+    if (!amount || !category || !date || !description) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+
     const income = await Income.create({ amount, category, date, description });
     res.status(201).json({ message: "Income added" });
   } catch (error) {
@@ -13,11 +17,28 @@ exports.addIncome = async (req, res) => {
   }
 };
 
-exports.getIncome = async(req, res) => {
-
+exports.getIncomes = async (req, res) => {
   try {
-    
+    const income = await Income.findAll({
+      order: [["createdAt", "DESC"]],
+    });
+    res.status(200).json(income);
   } catch (error) {
-    
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
   }
-}
+};
+
+exports.deleteIncome = async (req, res) => {
+  try {
+    const income = await Income.findByPk(req.params.id);
+    if (!income) {
+      return res.status(404).json({ message: "Income not found" });
+    }
+    await income.destroy();
+    res.json({ message: "Income deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/routes/transactionsRoutes.js
+++ backend/routes/transactionsRoutes.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-const { addIncome } = require("../controllers/incomeController");
+const {
+  addIncome,
+  getIncomes,
+  deleteIncome,
+} = require("../controllers/incomeController");
 
-router.post("/add-income", addIncome);
+const {
+  addExpense,
+  getExpenses,
+  deleteExpense,
+} = require("../controllers/expenseControllers");
+
+router
+  .post("/add-income", addIncome)
+  .get("/get-incomes", getIncomes)
+  .delete("/delete-income/:id", deleteIncome);
+
+router
+  .post("/add-expense", addExpense)
+  .get("/get-expenses", getExpenses)
+  .delete("/delete-expense/:id", deleteExpense);
 
 module.exports = router;
